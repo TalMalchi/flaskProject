d61a544882855a6ec2428fbddc83598eb99d23b0
Tal <malchital@gmail.com>
2022-11-04 13:03:56 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,8 +19,8 @@ if [ $machine == "test" ]
 then
     echo "Deploying to test server"
     scp -o StrictHostKeyChecking=no -r /var/lib/jenkins/workspace/* ec2-user@test:~
-    ssh -o StrictHostKeyChecking=no $USER@test "cd $HOME_DIR/Flask-app-AWS && docker-compose build && docker-compose up -d"
-    curl http://localhost:5005
+    ssh -o StrictHostKeyChecking=no $USER@test "cd $HOME_DIR/Flask-app-AWS && docker-compose build && docker-compose up"
+    ##curl http://localhost:5005
 elif [ $machine == "prod" ]
 then
     echo "Deploying to production server"
